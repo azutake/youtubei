cd23fa3a87a0a2fd5bb5795e4270cb438aa1f2bd
azutake <azutake@users.noreply.github.com>
2024-07-06 03:03:21 +0000

    Build: (6cc5307) 1.5.3

--- dist/esm/youtube/BaseVideo/BaseVideoParser.js
+++ dist/esm/youtube/BaseVideo/BaseVideoParser.js
@@ -18,7 +18,7 @@ var BaseVideoParser = /** @class */ (function () {
     function BaseVideoParser() {
     }
     BaseVideoParser.loadBaseVideo = function (target, data) {
-        var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18;
+        var _a, _b, _c;
         var videoInfo = BaseVideoParser.parseRawData(data);
         // Basic information
         target.id = videoInfo.videoDetails.videoId;
@@ -28,7 +28,7 @@ var BaseVideoParser = /** @class */ (function () {
         target.isLiveContent = videoInfo.videoDetails.isLiveContent;
         target.thumbnails = new Thumbnails().load(videoInfo.videoDetails.thumbnail.thumbnails);
         // Channel
-        var _19 = videoInfo.owner.videoOwnerRenderer, title = _19.title, thumbnail = _19.thumbnail, subscriberCountText = _19.subscriberCountText;
+        var _d = videoInfo.owner.videoOwnerRenderer, title = _d.title, thumbnail = _d.thumbnail, subscriberCountText = _d.subscriberCountText;
         target.channel = new BaseChannel({
             client: target.client,
             id: title.runs[0].navigationEndpoint.browseEndpoint.browseId,
@@ -49,16 +49,10 @@ var BaseVideoParser = /** @class */ (function () {
             target.related.items = BaseVideoParser.parseRelatedFromSecondaryContent(secondaryContents, target.client);
             target.related.continuation = getContinuationFromItems(secondaryContents);
         }
-        target.gameChannelId =
-            ((_o = (_m = (_l = (_k = (_j = (_h = (_g = (_f = (_e = (_d = videoInfo.metadataRowContainer) === null || _d === void 0 ? void 0 : _d.metadataRowContainerRenderer) === null || _e === void 0 ? void 0 : _e.rows) === null || _f === void 0 ? void 0 : _f.at(0)) === null || _g === void 0 ? void 0 : _g.richMetadataRowRenderer) === null || _h === void 0 ? void 0 : _h.contents) === null || _j === void 0 ? void 0 : _j.at(0)) === null || _k === void 0 ? void 0 : _k.richMetadataRenderer) === null || _l === void 0 ? void 0 : _l.endpoint) === null || _m === void 0 ? void 0 : _m.browseEndpoint) === null || _o === void 0 ? void 0 : _o.browseId) || null;
-        target.gameName = (_y = (_x = (_w = (_v = (_u = (_t = (_s = (_r = (_q = (_p = videoInfo.metadataRowContainer) === null || _p === void 0 ? void 0 : _p.metadataRowContainerRenderer) === null || _q === void 0 ? void 0 : _q.rows) === null || _r === void 0 ? void 0 : _r.at(0)) === null || _s === void 0 ? void 0 : _s.richMetadataRowRenderer) === null || _t === void 0 ? void 0 : _t.contents) === null || _u === void 0 ? void 0 : _u.at(0)) === null || _v === void 0 ? void 0 : _v.richMetadataRenderer) === null || _w === void 0 ? void 0 : _w.title) === null || _x === void 0 ? void 0 : _x.simpleText) !== null && _y !== void 0 ? _y : null;
         // captions
         if (videoInfo.captions) {
             target.captions = new VideoCaptions({ client: target.client, video: target }).load(videoInfo.captions.playerCaptionsTracklistRenderer);
         }
-        target.gameChannelId =
-            ((_8 = (_7 = (_6 = (_5 = (_4 = (_3 = (_2 = (_1 = (_0 = (_z = videoInfo.metadataRowContainer) === null || _z === void 0 ? void 0 : _z.metadataRowContainerRenderer) === null || _0 === void 0 ? void 0 : _0.rows) === null || _1 === void 0 ? void 0 : _1.at(0)) === null || _2 === void 0 ? void 0 : _2.richMetadataRowRenderer) === null || _3 === void 0 ? void 0 : _3.contents) === null || _4 === void 0 ? void 0 : _4.at(0)) === null || _5 === void 0 ? void 0 : _5.richMetadataRenderer) === null || _6 === void 0 ? void 0 : _6.endpoint) === null || _7 === void 0 ? void 0 : _7.browseEndpoint) === null || _8 === void 0 ? void 0 : _8.browseId) || null;
-        target.gameName = (_18 = (_17 = (_16 = (_15 = (_14 = (_13 = (_12 = (_11 = (_10 = (_9 = videoInfo.metadataRowContainer) === null || _9 === void 0 ? void 0 : _9.metadataRowContainerRenderer) === null || _10 === void 0 ? void 0 : _10.rows) === null || _11 === void 0 ? void 0 : _11.at(0)) === null || _12 === void 0 ? void 0 : _12.richMetadataRowRenderer) === null || _13 === void 0 ? void 0 : _13.contents) === null || _14 === void 0 ? void 0 : _14.at(0)) === null || _15 === void 0 ? void 0 : _15.richMetadataRenderer) === null || _16 === void 0 ? void 0 : _16.title) === null || _17 === void 0 ? void 0 : _17.simpleText) !== null && _18 !== void 0 ? _18 : null;
         return target;
     };
     BaseVideoParser.parseRelated = function (data, client) {
